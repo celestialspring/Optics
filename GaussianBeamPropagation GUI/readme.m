364efbb9cb6a16397c351bdc345cf3c2fdebca3c
celestialspring <san.mat.phy@gmail.com>
2026-09-24 07:46:10 +0200

    Create readme.m

---
+++ GaussianBeamPropagation GUI/readme.m
@@ -0,0 +1,22 @@
+
+@author: SM
+"""
+
+# Gaussian Beam Propagation
+
+Under modules, the GaussianBeam python script contains a class for simulating Beam propagation and calculating the relevant parameters. An example script gives an example 
+of how beam behaves under single lens transformation based on ABCD matrices and q-parameter. 
+
+
+## Scripts
+- `GaussianBeam.py`: Class containing functions used for building a beam propagation simulation.
+- `Singlelens_gaussianbeal.py`: An example script that uses the Class script to build a single lens beam propagation simulation.
+## Python libraries 
+- numpy, matplotlib, sys
+
+## Example: Gaussian beam through a single lens
+All relevant inputs are given in mm.
+
+
+![Alt text](./Modules/Singlelens_example.png)
+
